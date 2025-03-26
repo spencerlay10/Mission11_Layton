b0839d466a050e4d719b18e10547871bea68f67b
spencerlay10 <sklay10@gmail.com>
2025-03-26 08:53:43 -0600

    cart summary edits

--- backend/Mission11_Layton/Mission11_Layton/obj/Debug/net8.0/Mission11_Layton.AssemblyInfo.cs
+++ backend/Mission11_Layton/Mission11_Layton/obj/Debug/net8.0/Mission11_Layton.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11_Layton")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f23a407deff519ef57c0d96a2f84a71870b22710")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98c305ebee53bd946a1e812b749a6dd4efa4d4bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11_Layton")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11_Layton")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- frontend/src/components/CartSummary.tsx
+++ frontend/src/components/CartSummary.tsx
@@ -3,19 +3,26 @@ import { useCart } from "../context/CartContext";
 
 // Shows a cart summary in the top corner of the screen
 const CartSummary = () => {
-    const navigate = useNavigate();
-    const { cart } = useCart();
-    const totalAmount = cart.reduce((sum, item) => sum + item.price*item.quantity, 0);
-  
-    return (
-      <div
-        className="cart-summary"
-        onClick={() => navigate("/cart")}
-      >
-        🛒 <strong>${totalAmount.toFixed(2)}</strong>
+  const navigate = useNavigate();
+  const { cart } = useCart();
+  const totalAmount = cart.reduce(
+    (sum, item) => sum + item.price * item.quantity,
+    0
+  );
+  const bookNum = cart.reduce((sum, item) => sum + item.quantity, 0);
+
+  return (
+    <>
+      <div className="cart-summary" onClick={() => navigate("/cart")}>
+        <div>
+          🛒 <strong>${totalAmount.toFixed(2)}</strong>
+        </div>
+        <div>
+          <strong>{bookNum} Books</strong>
+        </div>
       </div>
-    );
-  };
-  
-  export default CartSummary;
-  
+    </>
+  );
+};
+
+export default CartSummary;
