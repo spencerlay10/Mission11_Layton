b91006838ba630172f094e043a9ea6d73ad9fdc6
spencerlay10 <sklay10@gmail.com>
2025-04-02 13:00:59 -0600

    updated backend

--- backend/Mission11_Layton/Mission11_Layton/obj/Debug/net8.0/Mission11_Layton.AssemblyInfo.cs
+++ backend/Mission11_Layton/Mission11_Layton/obj/Debug/net8.0/Mission11_Layton.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11_Layton")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c4944966afb6be69eea8a62d5ed9ad862556826")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0982d2b656fd055a1950ea10001c718b0757d044")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11_Layton")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11_Layton")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
