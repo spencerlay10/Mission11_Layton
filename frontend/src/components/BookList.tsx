90e055ef3f7da6d1e6ac230accc8e28a5270dbae
spencerlay10 <sklay10@gmail.com>
2025-04-15 11:18:00 -0600

    button

--- frontend/src/components/BookList.tsx
+++ frontend/src/components/BookList.tsx
@@ -54,6 +54,7 @@ function BookList({ selectedCategories }: { selectedCategories: string[] }) {
   if (error) return <p className="text-red-500">Error: {error}</p>;
   return (
     <>
+      <button className="btn btn-primary mb-3" onClick={() => navigate("/adminbooks")}></button>
       <div className="container mt-5">
         {/* Sorting controls */}
         <div className="d-flex justify-content-between mb-4 gap-3">
