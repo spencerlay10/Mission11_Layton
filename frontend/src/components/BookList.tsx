fd5a24415e7c48c67cf6d2b1dfe6b00dbc49e31b
spencerlay10 <sklay10@gmail.com>
2025-04-15 11:23:32 -0600

    reformat

--- frontend/src/components/BookList.tsx
+++ frontend/src/components/BookList.tsx
@@ -54,8 +54,8 @@ function BookList({ selectedCategories }: { selectedCategories: string[] }) {
   if (error) return <p className="text-red-500">Error: {error}</p>;
   return (
     <>
-      <button className="btn btn-primary mb-3" onClick={() => navigate("/adminbooks")}>Admin Page</button>
       <div className="container mt-5">
+      <button className="btn btn-primary mb-3" onClick={() => navigate("/adminbooks")}>Admin Page</button>
         {/* Sorting controls */}
         <div className="d-flex justify-content-between mb-4 gap-3">
           <div>
